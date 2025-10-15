fcc5176ce3adfb7c1a659ec91e90609027c830cd
pxxja <poojabhambid2004@gmail.com>
2025-10-15 11:44:18 +0530

    create .env file

--- Day1/server.js
+++ Day1/server.js
@@ -1,15 +1,17 @@
 import express from 'express';
+import 'dotenv/config';             
 
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.json("Hello");
     // console.log(req);
-    console.log(res)
+    res.send(`Hello ${process.env.NAME}! Welcome to the express server`)
+    // console.log(res)
+    // console.log(`Hello,${process.env.NAME}`);
 })
 
 app.get('/name',(req,res)=>{
-    res.json("Pooja");
+    res.send("Pooja");
 })
 
 app.listen(8000,()=>{
