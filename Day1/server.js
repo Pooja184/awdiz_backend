3909327275655676a88a765ab45d233514832778
pxxja <poojabhambid2004@gmail.com>
2025-10-20 23:36:07 +0530

    fix bug

--- Day1/server.js
+++ Day1/server.js
@@ -1,38 +1,38 @@
-import express from 'express';
-import 'dotenv/config';             
+import express from "express";
+import "dotenv/config";
 
-const app=express();
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-let users=[];
-app.get('/',(req,res)=>{
-    // console.log(req);
-    res.send(`Hello ${process.env.NAME}! Welcome to the express server`)
-    // console.log(res)
-    // console.log(`Hello,${process.env.NAME}`);
-})
+let users = [];
+app.get("/", (req, res) => {
+  // console.log(req);
+  res.send(`Hello ${process.env.NAME}! Welcome to the express server`);
+  // console.log(res)
+  // console.log(`Hello,${process.env.NAME}`);
+});
 
-app.post('/register',(req,res)=>{
-   try {
-     // console.log(req.body);
-     const {email,password}=req.body;
-     // console.log(users,"users");
-     const existedUser=users.find(user=>user.email===email);
-     if(existedUser){
-         res.send("User already exist");
-     }
-     users.push({email,password});
-     res.send("Registered successfully")
-   } catch (error) {
-    console.log(error)
-   }
-})
-console.log(users)
-app.get('/name',(req,res)=>{
-    res.send("Pooja");
-})
+app.post("/register", (req, res) => {
+  try {
+    // console.log(req.body);
+    const { email, password } = req.body;
+    // console.log(users,"users");
+    const existedUser = users.find((user) => user.email === email);
+    if (existedUser) {
+      res.send("User already exist");
+    }
+    users.push({ email, password });
+    res.send("Registered successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
+console.log(users);
+app.get("/name", (req, res) => {
+  res.send("Pooja");
+});
 
-app.listen(8000,()=>{
-    console.log(`app is listening on port ${8000}`);
-})
+app.listen(8000, () => {
+  console.log(`app is listening on port ${8000}`);
+});
