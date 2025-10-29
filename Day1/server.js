74698a99f2e9fb040249244497e864ec3253fcb3
pxxja <poojabhambid2004@gmail.com>
2025-10-29 12:48:39 +0530

    comment previous code

--- Day1/server.js
+++ Day1/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import mainRouter from "./routes/index.js";
-
+import mongoose from "mongoose";
 const app = express();
 
 app.use(express.json());
@@ -14,22 +14,22 @@ app.get("/", (req, res) => {
   // console.log(`Hello,${process.env.NAME}`);
 });
 
-app.post("/register", (req, res) => {
-  try {
-    // console.log(req.body);
-    const { email, password } = req.body;
-    // console.log(users,"users");
-    const existedUser = users.find((user) => user.email === email);
-    if (existedUser) {
-      res.send("User already exist");
-    }
-    users.push({ email, password });
-    res.send("Registered successfully");
-  } catch (error) {
-    console.log(error);
-  }
-});
-console.log(users);
+// app.post("/register", (req, res) => {
+//   try {
+//     // console.log(req.body);
+//     const { email, password } = req.body;
+//     // console.log(users,"users");
+//     const existedUser = users.find((user) => user.email === email);
+//     if (existedUser) {
+//       res.send("User already exist");
+//     }
+//     users.push({ email, password });
+//     res.send("Registered successfully");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+// console.log(users);
 app.get("/name", (req, res) => {
   res.send("Pooja");
 });
