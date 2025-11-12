4131ec5db663e7b1c602dcde182112f263ee81dd
pxxja <poojabhambid2004@gmail.com>
2025-11-12 11:51:22 +0530

    product routes

--- Day1/routes/products.routes.js
+++ Day1/routes/products.routes.js
@@ -1,10 +1,14 @@
 import {Router} from 'express';
+import { addProduct, deleteProduct, getAllProducts, updateProduct } from '../controllers/product.controller.js';
+import { tokenDecoder } from '../middlewares/tokenMiddleware.js';
 
 const productRouter=Router();
 
 
-productRouter.get('/products',(req,res)=>{
-    res.send("Hello! This is get Product API");
-})
+productRouter.get('/products',getAllProducts)
 
+productRouter.post('/add-product',addProduct)
+
+productRouter.put("/:id", tokenDecoder, updateProduct);
+productRouter.delete("/:id", tokenDecoder, deleteProduct);
 export default productRouter;
